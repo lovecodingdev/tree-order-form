28c7a6c2ccf1ac6457d748fc06fa15f369cafbde
lovecoding <lovecodingdev@gmail.com>
2022-02-20 01:17:10 +0300

    with sample data

--- components/TreeForm.vue
+++ components/TreeForm.vue
@@ -1,13 +1,26 @@
 <template>
   <div class="tree-form">
-    <div :style="indent" @click="toggleChildren">{{ label }}</div>
+    <div :style="indent" class="cursor-pointer flex">
+      <input v-if="tree.file" type="checkbox" :checked="checked" />
+      <span v-if="!tree.file">&bull;</span>
+      <span class="label" @click="toggleChildren">{{ tree.label }}</span>
+      <div v-if="tree.lastFolder" class="right">
+        <input
+          :id="'check_all_' + tree.key"
+          v-model="checkAll"
+          type="checkbox"
+          @click="toggleCheck"
+        />
+        <label :for="'check_all_' + tree.key">Check All</label>
+      </div>
+    </div>
     <div v-if="showChildren">
       <tree-form
-        v-for="node in nodes"
-        :key="node.label"
-        :nodes="node.nodes"
-        :label="node.label"
+        v-for="node in tree.nodes"
+        :key="node.key"
+        :tree="node"
         :depth="depth + 1"
+        :checked="checkAll"
       />
     </div>
   </div>
@@ -17,19 +30,45 @@ import { defineComponent } from '@nuxtjs/composition-api'
 
 export default defineComponent({
   name: 'TreeForm',
-  props: ['label', 'nodes', 'depth'],
+  props: ['tree', 'depth', 'checked'],
   data() {
-    return { showChildren: false }
+    return {
+      showChildren: false,
+      checkAll: false
+    }
   },
   computed: {
     indent(): Object {
-      return { transform: `translate(${this.depth * 50}px)` }
+      return { padding: `0 0 0 ${this.depth * 24}px` }
     }
   },
   methods: {
     toggleChildren() {
       this.showChildren = !this.showChildren
+    },
+    toggleCheck() {
+      // console.log(this.checkAll)
     }
   }
 })
 </script>
+
+<style scoped>
+.tree-form {
+  width: 960px;
+  background: #dee9f7;
+  margin: auto;
+}
+.cursor-pointer {
+  cursor: pointer;
+}
+.right {
+  float: right;
+}
+.flex {
+  display: flex;
+}
+.label {
+  flex: 1;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,50 +1,71 @@
 <template>
-  <div>
-    <div>Hello World!</div>
-    <tree-form :label="tree.label" :nodes="tree.nodes" :depth="0" />
+  <div class="page">
+    <tree-form :tree="tree" :depth="0" />
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from '@nuxtjs/composition-api'
 import TreeForm from '~/components/TreeForm.vue'
-let tree = {
-  label: 'root',
-  nodes: [
-    {
-      label: 'item1',
-      nodes: [
-        {
-          label: 'item1.1'
-        },
-        {
-          label: 'item1.2',
-          nodes: [
-            {
-              label: 'item1.2.1'
-            }
-          ]
-        }
-      ]
-    },
-    {
-      label: 'item2'
-    }
-  ]
+
+interface File {
+  path: string
+  url: string
+  size: string
+}
+
+interface Tree {
+  label: string
+  nodes?: Tree[]
+  lastFolder?: boolean
+  file?: File
+  key: string
 }
 
 export default defineComponent({
   components: { TreeForm },
   data: () => ({
-    files: [],
-    tree
+    files: [] as File[],
+    tree: {} as Tree
   }),
   async fetch() {
     this.files = await fetch(`${window.location.href}/files.json`).then((res) =>
       res.json()
     )
+    let tree = [] as Tree[]
+    for (const file of this.files) {
+      let segments = file.path.split('/')
+      let fileName = segments.pop()
+      let treeFile = tree
+      let key = ''
+      segments.forEach((seg, segIndex) => {
+        key = key === '' ? seg : key + '-' + seg
+        let node = treeFile.find((node) => node.label === seg)
+        if (!node) {
+          treeFile.push({
+            label: seg,
+            nodes: [],
+            lastFolder: segIndex === segments.length - 1,
+            key
+          })
+          node = treeFile[treeFile.length - 1]
+        }
+        treeFile = node.nodes || []
+      })
+      treeFile.push({
+        label: `${fileName} (${file.size})`,
+        file,
+        key: key + '-' + fileName
+      })
+    }
+    console.log(tree)
+    this.tree = tree[0]
   }
 })
 </script>
 
-<style scoped></style>
+<style scoped>
+.page {
+  padding: 48px;
+}
+</style>
