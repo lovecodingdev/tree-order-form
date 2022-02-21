3dd4e215d3c4ac84ec50e53537d73445a896d01f
lovecoding <lovecodingdev@gmail.com>
2022-02-21 04:04:22 +0300

    Downloader component

---
+++ components/Downloader.vue
@@ -0,0 +1,79 @@
+<template>
+  <div class="downloader">
+    <tree-form :tree="tree" :depth="0" />
+    <button class="download-button" @click="onDownload">Download</button>
+  </div>
+</template>
+<script lang="ts">
+import { defineComponent } from '@nuxtjs/composition-api'
+import TreeForm from '~/components/TreeForm.vue'
+import { File, Tree } from '~/utils/types'
+
+export default defineComponent({
+  name: 'Downloader',
+  components: { TreeForm },
+  props: ['files'],
+  data() {
+    return {
+      tree: {} as Tree
+    }
+  },
+  computed: {},
+  watch: {
+    files(newVal, oldVal) {
+      this.getTreeData()
+    }
+  },
+  methods: {
+    getTreeData() {
+      let tree = [] as Tree[]
+      for (const file of this.files as File[]) {
+        let segments = file.path.split('/')
+        let fileName = segments.pop()
+        let treeFile = tree
+        let key = ''
+        segments.forEach((seg, segIndex) => {
+          key = key === '' ? seg : key + '-' + seg
+          let node = treeFile.find((node) => node.label === seg)
+          if (!node) {
+            treeFile.push({
+              label: seg,
+              nodes: [],
+              lastFolder: segIndex === segments.length - 1,
+              key
+            })
+            node = treeFile[treeFile.length - 1]
+          }
+          treeFile = node.nodes || []
+        })
+        treeFile.push({
+          label: `${fileName} (${file.size})`,
+          file,
+          key: key + '-' + fileName
+        })
+      }
+      console.log(tree)
+      this.tree = tree[0]
+    },
+    onDownload() {
+      confirm('Are you sure to download the files?')
+    }
+  }
+})
+</script>
+
+<style scoped>
+.downloader {
+  width: 960px;
+  background: #dee9f7;
+  margin: auto;
+}
+.download-button {
+  position: fixed;
+  right: calc((100% - 960px) / 2);
+  top: 48px;
+  background-color: #708edd;
+  color: white;
+  border-radius: 4px;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,65 +1,23 @@
 <template>
   <div class="page">
-    <tree-form :tree="tree" :depth="0" />
+    <Downloader :files="files" />
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from '@nuxtjs/composition-api'
-import TreeForm from '~/components/TreeForm.vue'
-
-interface File {
-  path: string
-  url: string
-  size: string
-}
-
-interface Tree {
-  label: string
-  nodes?: Tree[]
-  lastFolder?: boolean
-  file?: File
-  key: string
-}
+import Downloader from '~/components/Downloader.vue'
+import { File } from '~/utils/types'
 
 export default defineComponent({
-  components: { TreeForm },
+  components: { Downloader },
   data: () => ({
-    files: [] as File[],
-    tree: {} as Tree
+    files: [] as File[]
   }),
   async fetch() {
     this.files = await fetch(`${window.location.href}/files.json`).then((res) =>
       res.json()
     )
-    let tree = [] as Tree[]
-    for (const file of this.files) {
-      let segments = file.path.split('/')
-      let fileName = segments.pop()
-      let treeFile = tree
-      let key = ''
-      segments.forEach((seg, segIndex) => {
-        key = key === '' ? seg : key + '-' + seg
-        let node = treeFile.find((node) => node.label === seg)
-        if (!node) {
-          treeFile.push({
-            label: seg,
-            nodes: [],
-            lastFolder: segIndex === segments.length - 1,
-            key
-          })
-          node = treeFile[treeFile.length - 1]
-        }
-        treeFile = node.nodes || []
-      })
-      treeFile.push({
-        label: `${fileName} (${file.size})`,
-        file,
-        key: key + '-' + fileName
-      })
-    }
-    console.log(tree)
-    this.tree = tree[0]
   }
 })
 </script>
